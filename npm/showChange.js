4aaee7c7e65a193ade8876009c16adb51b91f1b3
Amos Wenger <amos@itch.io>
2017-08-29 23:05:09 +0200

    Add deep-diff support

--- npm/showChange.js
+++ npm/showChange.js
@@ -3,7 +3,99 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.style = style;
+exports.render = render;
 
-exports.default = function (name, key, before, after) {
-  console.log('%c' + name, 'font-weight: bold;', before[key] && before[key].toJS && '(immutable):' || '', before[key] ? before[key].toJS ? before[key].toJS() : before[key] : before[key], '->', after[key] && after[key].toJS && '(immutable):' || '', after[key] ? after[key].toJS ? after[key].toJS() : after[key] : after[key]);
+exports.default = function (name, key, beforeObj, afterObj) {
+  var before = beforeObj[key];
+  var after = afterObj[key];
+  console.log('%c' + name, 'font-weight: bold;');
+  diffLogger(before, after, console, false);
 };
+
+var _deepDiff = require('deep-diff');
+
+var _deepDiff2 = _interopRequireDefault(_deepDiff);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+// https://github.com/flitbit/diff#differences
+var dictionary = {
+  E: {
+    color: '#2196F3',
+    text: 'CHANGED:'
+  },
+  N: {
+    color: '#4CAF50',
+    text: 'ADDED:'
+  },
+  D: {
+    color: '#F44336',
+    text: 'DELETED:'
+  },
+  A: {
+    color: '#2196F3',
+    text: 'ARRAY:'
+  }
+};
+
+function style(kind) {
+  return 'color: ' + dictionary[kind].color + '; font-weight: bold';
+}
+
+function render(diff) {
+  var kind = diff.kind,
+      path = diff.path,
+      lhs = diff.lhs,
+      rhs = diff.rhs,
+      index = diff.index,
+      item = diff.item;
+
+
+  switch (kind) {
+    case 'E':
+      return [path.join('.'), lhs, '→', rhs];
+    case 'N':
+      return [path.join('.'), rhs];
+    case 'D':
+      return [path.join('.')];
+    case 'A':
+      return [path.join('.') + '[' + index + ']', item];
+    default:
+      return [];
+  }
+}
+
+function diffLogger(prevState, newState, logger, isCollapsed) {
+  var diff = (0, _deepDiff2.default)(prevState, newState);
+
+  try {
+    if (isCollapsed) {
+      logger.groupCollapsed('diff');
+    } else {
+      logger.group('diff');
+    }
+  } catch (e) {
+    logger.log('diff');
+  }
+
+  if (diff) {
+    diff.forEach(function (elem) {
+      var kind = elem.kind;
+
+      var output = render(elem);
+
+      logger.log.apply(logger, ['%c ' + dictionary[kind].text, style(kind)].concat(_toConsumableArray(output)));
+    });
+  } else {
+    logger.log('—— no diff ——');
+  }
+
+  try {
+    logger.groupEnd();
+  } catch (e) {
+    logger.log('—— diff end —— ');
+  }
+}
--- src/showChange.js
+++ src/showChange.js
@@ -1,19 +1,83 @@
-export default function (name, key, before, after) {
+import differ from 'deep-diff';
+
+// https://github.com/flitbit/diff#differences
+const dictionary = {
+  E: {
+    color: '#2196F3',
+    text: 'CHANGED:',
+  },
+  N: {
+    color: '#4CAF50',
+    text: 'ADDED:',
+  },
+  D: {
+    color: '#F44336',
+    text: 'DELETED:',
+  },
+  A: {
+    color: '#2196F3',
+    text: 'ARRAY:',
+  },
+};
+
+export function style(kind) {
+  return `color: ${dictionary[kind].color}; font-weight: bold`;
+}
+
+export function render(diff) {
+  const { kind, path, lhs, rhs, index, item } = diff;
+
+  switch (kind) {
+    case 'E':
+      return [path.join('.'), lhs, '→', rhs];
+    case 'N':
+      return [path.join('.'), rhs];
+    case 'D':
+      return [path.join('.')];
+    case 'A':
+      return [`${path.join('.')}[${index}]`, item];
+    default:
+      return [];
+  }
+}
+
+function diffLogger(prevState, newState, logger, isCollapsed) {
+  const diff = differ(prevState, newState);
+
+  try {
+    if (isCollapsed) {
+      logger.groupCollapsed('diff');
+    } else {
+      logger.group('diff');
+    }
+  } catch (e) {
+    logger.log('diff');
+  }
+
+  if (diff) {
+    diff.forEach((elem) => {
+      const { kind } = elem;
+      const output = render(elem);
+
+      logger.log(`%c ${dictionary[kind].text}`, style(kind), ...output);
+    });
+  } else {
+    logger.log('—— no diff ——');
+  }
+
+  try {
+    logger.groupEnd();
+  } catch (e) {
+    logger.log('—— diff end —— ');
+  }
+}
+
+export default function (name, key, beforeObj, afterObj) {
+  const before = beforeObj[key];
+  const after = afterObj[key];
   console.log(
     '%c' + name,
     'font-weight: bold;',
-    (before[key] && before[key].toJS && '(immutable):') || '',
-    before[key]
-      ? before[key].toJS
-        ? before[key].toJS()
-        : before[key]
-      : before[key],
-    '->',
-    (after[key] && after[key].toJS && '(immutable):') || '',
-    after[key]
-      ? (after[key].toJS
-        ? after[key].toJS()
-        : after[key])
-      : after[key]
   );
+  diffLogger(before, after, console, false);
 }
